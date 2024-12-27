42efbe4404b2e2a367b47bb5d257dfbd87dd1ed3
Alonso Trejo <alonsotrejo1970@hotmail.com>
2024-12-26 20:28:54 -0600

    commit en railway

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -140,9 +140,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATIC_ROOT= os.path.join(BASE_DIR,'static_files')
+
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -160,7 +164,7 @@ EMAIL_USE_TLS= True
 DEFAULT_FROM_EMAIL = 'literatus.com <layenaalonso@gmail.com>'
 
 
-ALLOWED_HOSTS = ['localhost', 'literatus-project-production.up.railway.app']
+ALLOWED_HOSTS = ['literatus-project-production.up.railway.app']
 
 
 
