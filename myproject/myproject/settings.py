d9b9b6ebd830534419c00b3674fdeff2e96d01d0
Alonso Trejo <alonsotrejo1970@hotmail.com>
2025-01-03 21:04:28 -0600

    probando railway url

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -163,7 +163,7 @@ DEFAULT_FROM_EMAIL = 'literatus.com <layenaalonso@gmail.com>'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-ALLOWED_HOSTS = ['localhost', 'literatustalleresycursos.com', 'literatus-project-production.up.railway.app/']
+ALLOWED_HOSTS = ['localhost', 'literatustalleresycursos.com', 'literatus-project-production.up.railway.app']
 
 
 CSRF_TRUSTED_ORIGINS = ['http://*', 'https://literatus-project-production.up.railway.app']
